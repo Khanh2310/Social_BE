a3dba4e449b69610165f55fe552e2780e17ab4b7
cnvs_quoc_bao <nguyenhoangquocbao.canvas@gmail.com>
2023-10-31 17:34:00 +0700

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../modules/userModules.js';
 import bcrypt from 'bcryptjs';
 import generateTokenAndSetCookie from '../utils/helpers/generateTokenAndSetCookie.js';
+import mongoose from 'mongoose';
 const signupUser = async (req, res) => {
   try {
     const { name, email, username, password } = req.body;
@@ -77,4 +78,111 @@ const logoutUser = async (req, res) => {
     console.log('Error in signupUser', err.message);
   }
 };
-export { signupUser, signInUser, logoutUser };
+
+const followUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id.toString())
+      return res
+        .status(400)
+        .json({ error: 'You cannot follow/unfollow yourself' });
+
+    if (!userToModify || !currentUser)
+      return res.status(400).json({ error: 'User not found' });
+
+    const isFollowing = currentUser.follwings.includes(id);
+
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { follwings: id } });
+      res.status(200).json({ message: 'User unfollowed successfully' });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { follwings: id } });
+      res.status(200).json({ message: 'User followed successfully' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log('Error in followUnFollowUser: ', err.message);
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { name, email, username, password, bio } = req.body;
+  let { profilePic } = req.body;
+  const userId = req.user._id;
+  try {
+    let user = await User.findById(userId);
+    if (!user)
+      return res.status(400).json({
+        error: 'User not found',
+      });
+    if (req.params.id !== userId.toString()) {
+      return res.status(400).json({
+        error: "You cannot update other user's profile",
+      });
+    }
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+    user = await user.save();
+
+    // password should be null in response
+    user.password = null;
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      error: err.message,
+    });
+    console.log('Error in updateUser', err.message);
+  }
+};
+const getUserProfile = async (req, res) => {
+  // We will fetch user profile either with username or userId
+  // query is either username or userId
+  const { query } = req.params;
+  try {
+    let user;
+    // query is userId
+    // -password is not get password
+    //-updatedAt  is not get updatedAt
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findOne({ _id: query })
+        .select('-password')
+        .select('-updatedAt');
+    } else {
+      // query is username
+      user = await User.findOne({
+        username: query,
+      })
+        .select('-password')
+        .select('-updatedAt');
+    }
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log('Error in getUserProfile: ', err.message);
+  }
+};
+export {
+  signupUser,
+  signInUser,
+  logoutUser,
+  followUser,
+  updateUser,
+  getUserProfile,
+};
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,12 +3,19 @@ import {
   signupUser,
   signInUser,
   logoutUser,
+  followUser,
+  updateUser,
+  getUserProfile,
 } from '../controllers/userController.js';
+import protectRoute from '../middlewares/protectRoute .js';
 
 const router = express.Router();
 
+router.get('/profile/:query', getUserProfile);
 router.post('/sign-up', signupUser);
 router.post('/sign-in', signInUser);
 router.post('/logout', logoutUser);
+router.post('/follow/:id', protectRoute, followUser);
+router.post('/update/:id', protectRoute, updateUser);
 
 export default router;
